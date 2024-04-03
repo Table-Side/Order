2882bc2ec893f861fac405dc60cedc267885d604
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-03 17:37:07 +0100

    Add `AuthenticatedRequest` interface and `DecodeJWT` middleware

---
+++ src/middleware/decode_jwt.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { AuthenticatedRequest } from '../interfaces';
+
+const decodeJWT = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (token) {
+        try {
+            const decoded = jwt.decode(token, { json: true });
+
+            if (!decoded) {
+                return res.status(401).json({
+                    error: {
+                        message: 'Invalid JWT token provided',
+                        details: 'Token could not be decoded'
+                    }
+                });
+            }
+
+            req.user = {
+                sub: decoded.sub,
+                name: decoded.name,
+                email: decoded.email,
+                verified: decoded.verified,
+                realm_access: {
+                    roles: decoded.realm_access.roles
+                }
+            }
+        } catch (error) {
+            return res.status(401).json({
+                error: {
+                    message: 'Invalid JWT token provided',
+                    details: error
+                }
+            });
+        }
+    }
+
+    next();
+};
+
+export default decodeJWT;
