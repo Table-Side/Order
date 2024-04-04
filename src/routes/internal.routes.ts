a7bc0b8629a0eae6bc11e2d6f65a003408ddb3f2
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-04 18:19:01 +0100

    Add internal request logic

---
+++ src/routes/internal.routes.ts
@@ -0,0 +1,31 @@
+import { Router, Request, Response } from "express";
+import prisma from "../config/prisma";
+
+const router = Router();
+
+
+router.get("/:orderId", async (req: Request, res: Response) => {
+    try {
+        const { orderId } = req.params;
+
+        const order = await prisma.order.findUnique({
+            where: {
+                id: orderId,
+            },
+        });
+
+        if (!order) {
+            return res.status(404).json({ error: "Order not found" });
+        }
+
+        res.status(200).json({
+            data: order
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to remove item from order" });
+    }
+});
+
+
+
+export default router;
