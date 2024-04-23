501af54dcdd43fe0fab3b3364be98012b5ea823a
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-23 19:02:55 +0100

    Remove middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,5 @@
 export { default as decodeJWT } from "./decode_jwt"; 
 export { default as isAuthenticated } from "./authenticated";
 export { default as hasRole } from "./has_role";
-export { default as isRestaurantOwner } from "./is_restaurant_owner";
 export { default as isOrderForUser } from "./is_order_for_user";
 export { default as restaurantExists } from "./restaurant_exists";
