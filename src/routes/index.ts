717034ac3505894c5d9b108f0d49112a16d998ac
George Nick Gorzynski <georgegorzynski@me.com>
2024-04-24 13:57:40 +0100

    Remove dead reference

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,3 @@
 export { default as order } from "./order.routes"; 
 export { default as internal } from "./internal.routes";
 export { default as user } from "./user.routes";
-export { default as orderItems } from "./order-items.routes";
